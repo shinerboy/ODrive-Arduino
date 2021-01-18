08a9132df1acf7bf7d32782bc479cba73f7bf7fd
[shinerboy] <[ernestoh_92@hotmail.com]>
2021-01-18 17:27:39 -0600

    Modified ODriveArduinoTest05 to make it simple to read

--- ODriveArduintoTest04/ODriveArduintoTest05/ODriveArduintoTest05.ino
+++ ODriveArduintoTest04/ODriveArduintoTest05/ODriveArduintoTest05.ino
@@ -52,6 +52,8 @@ void setup() {
 
 void loop() {
 
+  ///////////////////////// CALIBRATION //////////////////////////
+
   if (Serial.available()) {
     char c = Serial.read();
 
@@ -95,125 +97,9 @@ void loop() {
 
       Serial.println("DONE!");
     }
-    
- 
-
-    // Sinusoidal test move
-    if (c == 's') {
-      char test = Serial.read();
-      Serial.println("Executing test move");
-      float ph = 0.0f;
-      while (test != 't')
-      { 
-        float float_speed = speed_init/1000.0;
-        ph += 0.001f * speed_init;
-        test = Serial.read();
-        float pos_m0 = 5000.f * cos(ph) * (amp_init/360); //20000.0f = 360 degrees. 
-        //float pos_m1 = 20000.0f * sin(ph);
-        odrive.SetPosition(0, pos_m0);
-        //odrive.SetPosition(1, pos_m1);
-        delay(5);
-      }
-      Serial.println("Stopped");
-    }
-
-    // Read bus voltage
-    if (c == 'b') {
-      odrive_serial << "r vbus_voltage\n";
-      Serial << "Vbus voltage: " << odrive.readFloat() << '\n';
-    }
-
-    if (c == 'u') {
-      Serial.println("Set speed: ");
-      //Speed matched gait at speed of 40
-      String sp = Serial.readString();
-      String sp2 = sp;
-      Serial.println(sp);
-
-      while(sp == sp2)
-      {
-        sp = Serial.readString();
-        //sp = sp.toInt();
-        Serial.println(sp);
-      }
-
-      sp = sp.toInt();
-
-      Serial.print("Set speed to: ");
-      Serial.print(sp);
-      Serial.println(" ?(y)(n)");
-      char yn = Serial.read();
-      while (yn != 'y')
-        {
-          yn = Serial.read();
-          if (yn == 'n')
-          {
-            sp = speed_init;
-            Serial.println("Speed not changed");
-            break;
-            }
-          if (yn == 'y')
-          {
-            Serial.println("Succesfully set speed!");
-            speed_init = sp.toInt();
-            }
-          delay(5);
-        }
-     }
-
-     if (c == 'a') {
-      Serial.println("Set amplitude (degrees): ");
-      String amp = Serial.readString();
-      String amp2 = amp;
-      Serial.println(amp);
-
-      while(amp == amp2)
-      {
-        amp = Serial.readString();
-        //sp = sp.toInt();
-        Serial.println(amp);
-      }
-
-      amp = amp.toInt();
-
-      Serial.print("Set amplitude to: ");
-      Serial.print(amp);
-      Serial.println(" ?(y)(n)");
-      char yn = Serial.read();
-      while (yn != 'y')
-        {
-          yn = Serial.read();
-          if (yn == 'n')
-          {
-            amp = amp_init;
-            Serial.println("Amplitude not changed");
-            break;
-            }
-          if (yn == 'y')
-          {
-            Serial.println("Succesfully set amplitude!");
-            amp_init = amp.toInt();
-            }
-          delay(5);
-        }
-     }
-      
-    
-    
-
-    // print motor positions in a 10s loop
-    if (c == 'p') {
-      static const unsigned long duration = 10000;
-      unsigned long start = millis();
-      while(millis() - start < duration) {
-        for (int motor = 0; motor < 2; ++motor) {
-          odrive_serial << "r axis" << motor << ".encoder.pos_estimate\n";
-          Serial << odrive.readFloat() << '\t';
-        }
-        Serial << '\n';
-      }
-    }
-  }
+    ////////////////////////////END CALIBRATION
+  }   
+ ///////////////// BEGIN TORQUE CONTROL CODE
    delay(500);
 
   time_diff = 0;
@@ -223,13 +109,15 @@ void loop() {
     Serial.println(time_diff);
     Serial.println("Before");
 
+    //SET TORQUE VALUE TO 1.0 for 1000ms
+
     while (time_diff < 1000){
       time_diff = millis()-time_start;
       Serial.println("HI");
       Serial.println(time_diff);
       odrive_serial << "w axis" << 0 << ".controller.config.vel_limit " << 20.0f << '\n'; //4.0f
     odrive_serial << "w axis" << 0 << ".motor.config.current_lim " << 10.0f << '\n'; //44.0f
-      odrive_serial << "c " << 0 << " " << 1.0f << "\n";
+      odrive_serial << "c " << 0 << " " << 1.0f << "\n"; //Change 1.0f to other torque value
       }
       Serial.println("Bye");
 
@@ -241,14 +129,14 @@ void loop() {
 
     Serial.println(time_diff);
     Serial.println("Before");
-
+//SET TORUQE VALUE to 5.0 for 100ms
     while (time_diff < 100){
       time_diff = millis()-time_start;
       Serial.println("HI");
       Serial.println(time_diff);
       odrive_serial << "w axis" << 0 << ".controller.config.vel_limit " << 80.0f << '\n'; //4.0f
     odrive_serial << "w axis" << 0 << ".motor.config.current_lim " << 10.0f << '\n'; //44.0f
-    odrive_serial << "c " << 0 << " " << 5.0f << "\n";
+    odrive_serial << "c " << 0 << " " << 5.0f << "\n"; //CHANGE 5.0f to other torque value
       }
       Serial.println("Bye");
 }
